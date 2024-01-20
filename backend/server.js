c9b6f8ab189074c2d708d569349a91ac70f23843
sheikh-jilani <jilanesk7@gmail.com>
2024-01-21 02:23:40 +0600

    3rd update commit

--- backend/server.js
+++ backend/server.js
@@ -57,9 +57,9 @@ app.use("/api/orders", orderRoute);
 app.use("/api/upload", uploadFileRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
